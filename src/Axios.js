01014c0875e6737e3d78d38f1cb92d80e79630e2
Robert Kirakosyan <kirakosyanrobert2018@gmail.com>
2019-11-15 20:43:58 +0400

    aaa

--- src/Axios.js
+++ src/Axios.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-const AUTH_TOKEN = "d8b7a350d87e4fb69cb4953b532738cd";
+const AUTH_TOKEN = "d8b7a350d87e4fb69cb4953b532738cd" ;
 
 const Axios = axios.create({
     baseURL: 'https://newsapi.org/v2/'

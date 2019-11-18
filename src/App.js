042dc945d293fb05eccd42de6f89a78f55bd29c7
kirakosyanrobert <kirakosyanrobert2018@gmail.com>
2019-11-18 23:02:22 +0400

    aaa

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import React, {Component} from 'react';
+import React from 'react';
 import {Route} from 'react-router-dom';
 import './App.scss';
 import Header from './Components/Header/Header';
 import Footer from './Components/Footer/Footer';
 import Home from './Components/Home/Home';
 
-class App extends Component {
-  
-  render() {
-      return (
-        <>
-          <Header />
-          <h1>Gagas from Petrovsk</h1>
-          <div className="page-wrapper">
-            <Route path="/" component={Home} />
-          </div>
-          <Footer />
-        </>
-      );
-  }
-
+const App = () => {
+    return (
+      <>
+        <Header />
+        <div className="page-wrapper">
+          <Route path="/" component={Home} />
+        </div>
+        <Footer />
+      </>
+    );
 }
+
 
 export default App;
--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,27 +1,25 @@
-import React, {Component} from 'react';
+import React from 'react';
 import './Header.scss';
 
-class Header extends Component {
-    render() {
+const Header = () => {
         return (
             <header>
                 <nav className="header__wrapper">
                     <div className="header__logo">
-                        <h2>News Blog</h2>
+                        <h2>Logo</h2>
                     </div>
                     <div className="header__navigation">
                         <ul>
-                            <li><a href="">GAGAS</a></li>
-                            <li><a href="">GAGAS</a></li>
-                            <li><a href="">GAGAS</a></li>
-                            <li><a href="">GAGAS</a></li>
-                            <li><a href="">GAGAS</a></li>
+                            <li><a href="#">GAGAS</a></li>
+                            <li><a href="#">GAGAS</a></li>
+                            <li><a href="#">GAGAS</a></li>
+                            <li><a href="#">GAGAS</a></li>
+                            <li><a href="#">GAGAS</a></li>
                         </ul>
                     </div>
                 </nav>
             </header>
         )
     }
-}
 
 export default Header;
